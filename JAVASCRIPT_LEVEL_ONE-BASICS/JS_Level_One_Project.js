24b962c04da9edb4f155ccfd2a1d1d2d88691620
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2023-03-04 23:04:12 +0300

    Fullstack web dev

--- JAVASCRIPT_LEVEL_ONE-BASICS/JS_Level_One_Project.js
+++ JAVASCRIPT_LEVEL_ONE-BASICS/JS_Level_One_Project.js
@@ -1,1 +1,47 @@
- 
+let firstName = prompt("Enter your first name: ");
+let lasttName = prompt("Please enter your last name: ");
+let age = prompt("How old are you? ");
+let height = prompt("How tall are you in centimeters? ");
+let pet = prompt("What is the name of your pet? ");
+alert("Thank you so much for the information!");
+
+let nameCond = null;
+let ageCond = null;
+let heightCond = null;
+let petCond = null;
+
+// Name condition
+if (firstName[0] === lasttName[0]) {
+    nameCond = true
+} else {
+    nameCond = false
+}
+
+// Age condition
+if (age > 20 && age < 30) {
+    ageCond = true
+} else {
+    ageCond = false
+}
+
+// Height condition
+if (height >= 170) {
+    heightCond = true
+} else {
+    ceightCond = false
+}
+
+// Pet condition
+if (pet[pet.length-1] === "y") {
+    pettCond = true
+} else {
+    petCond = false
+}
+
+// Check all conditions
+if (nameCond && ageCond  && heightCond && petCond) {
+    console.log("Access Granted!");
+} else {
+    console.log("Access Denied!");
+}
+
