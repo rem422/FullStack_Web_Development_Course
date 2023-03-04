23a84ae48df14172c6effe2c837c83de4b8f876f
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2023-03-04 12:55:27 +0300

    Fullstack Web Dev

---
+++ JAVASCRIPT_LEVEL_ONE-BASICS/main.js
@@ -0,0 +1,3 @@
+let deposit = prompt("How much would you like to deposit today? ")
+alert("You've deposited " + deposit);
+console.log("You are a cool person!");
---
+++ JAVASCRIPT_LEVEL_ONE-BASICS/part3.js
@@ -0,0 +1,4 @@
+let pounds = prompt("What is the weight in pounds (lbs)? ");
+let kg = 0.454;
+alert("That is: " + pounds * kg + " kilograms");
+console.log("Conversion Completed");
